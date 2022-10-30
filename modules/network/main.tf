eacc0510a0b1da22f8dd5c711afc92dffb896cfd
Brian <b.matthew.chadwick@gmail.com>
2022-10-30 16:25:51 -0400

    VPC created

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -5,6 +5,11 @@ provider "aws" {
 
 resource "aws_vpc" "this" {
   cidr_block = "10.0.0.0/16"
+
+  tags = {
+    "Name" = "tf-ftw"
+
+  }
 }
 
 terraform {
@@ -12,3 +17,6 @@ terraform {
     key = "modules/network/terraform.tfstate"
   }
 }
+
+# NACLs
+# SGs
