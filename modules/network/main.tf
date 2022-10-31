4ba899e1b6ef90cf57f8e27d516c0320c623158f
Brian <b.matthew.chadwick@gmail.com>
2022-10-30 23:35:26 -0400

    Route to NGW working

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -23,6 +23,11 @@ resource "aws_subnet" "private" {
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.this.id
 
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.this.id
+  }
+
   tags = {
     "Name" = "${var.project}-route-table-private-${var.env}"
   }
