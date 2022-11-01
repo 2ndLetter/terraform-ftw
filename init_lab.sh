ebb32d9e3d9630fe5d1f4abd997efdd3cfefb702
Brian <b.matthew.chadwick@gmail.com>
2022-11-01 05:30:29 -0400

    Updated comments for script

--- init_lab.sh
+++ init_lab.sh
@@ -15,7 +15,7 @@
 #   Use to delete prerequisites from an AWS account.  #
 #                                                     #
 # Clean up the lab: ./init_lab.sh -c                  #
-#   Use to delete files some Terraform files.         #
+#   Use to delete some Terraform files.               #
 #   Only run if Cloud Sandbox is already deleted.     #
 #######################################################
 
